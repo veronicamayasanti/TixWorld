3925e4ee8e127b873b37525929bff2811e32be39
Veronica Maya Santi <veronicamayasanti@gmail.com>
2024-08-12 12:58:30 +0700

    router index find update

--- BE/app/api/v1/categories/controller.js
+++ BE/app/api/v1/categories/controller.js
@@ -1,8 +1,8 @@
 import Categories from './model.js';
 
-const create = async (req, res, next) => {
+export const create = async (req, res, next) => {
     try {
-        const  { name } = req.body;
+        const { name } = req.body;
         const result = await Categories.create({ name });
         res.status(201).json({
             message: 'Category created successfully',
@@ -14,5 +14,51 @@ const create = async (req, res, next) => {
     }
 }
 
-export default create
+export const index = async (req, res, next) => {
+    try {
+        const result = await Categories.find().select('_id name');
+        res.status(200).json({
+            message: 'Category fetched successfully',
+            data: result
+        });
+    } catch (error) {
+        console.error("Error fetching category:", error.message);
+        next(error);
+    }
+}
 
+export const find = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const result = await Categories.findOne({ _id: id });
+        if (!result) return res.status(404).json({
+            message: 'Category not found'
+        })
+        res.status(200).json({
+            data: result
+        });
+    } catch (error) {
+        next(error);
+    }
+
+}
+
+export const update = async (req, res, next) => {
+    try {
+        const { id } = req.params;
+        const { name } = req.body;
+        const result = await Categories.findOneAndUpdate(
+            { 
+                _id: id 
+            },
+            { name },
+            { new: true, rupsert: true }
+        );
+        res.status(200).json({
+            data: result,
+            message: 'Category updated successfully'
+        });
+    } catch (error) {
+        next(error);
+    }
+}
--- BE/app/api/v1/categories/router.js
+++ BE/app/api/v1/categories/router.js
@@ -1,20 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import  create from './controller.js';
+import  { create, index, find, update } from './controller.js';
 
-router.get('/categories', (req, res) => {
-    const data = [
-        {
-            _id: '352343',
-            name: 'TixWorld'
-        }
-    ];
-    res.status(200).json({
-        message: 'Halaman categories TixWorld',
-        data : data
-    })
-})
-
+router.get('/categories', index)
+router.get('/categories/:id', find)
+router.put('/categories/:id', update)
 router.post('/categories', create)
 
 
